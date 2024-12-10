66eafb56f0309c6f66819abb970e906a43cda8e0
Ali Ibragimov <152302855+AliIbragimov@users.noreply.github.com>
2024-12-10 19:41:25 +0300

    Add files via upload

---
+++ res/script/script.js
@@ -0,0 +1,34 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const btnUp = {
+    el: document.querySelector('.scroll'),
+    show() {
+      this.el.classList.remove('scroll_hide');
+    },
+    hide() {
+      this.el.classList.add('scroll_hide');
+    },
+    addEventListener() {
+      window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      document.querySelector('.scroll').onclick = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
